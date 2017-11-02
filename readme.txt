e274b7fe2ed636d11a80b5076a975c7e198de09f
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:27:51 -0400

    First commit

---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: LAMP 40 - Final Classes
+Published: 11/13/2012 5:05:39 PM
+Link: https://www.youtube.com/watch?v=OBrpIfnw9Uo
+
+Description: Learn how to create unchangeable classes
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
---
+++ tut40.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<title>Untitled</title>
+</head>
+
+<body>
+
+<?php
+
+final class test
+{
+	function DoTest()
+	{
+		//do something special
+	}
+}
+
+class NewTest extends test
+{
+	function DoTest()
+	{
+		//do something different!
+	}
+}
+
+?>
+
+</body>
+</html>
